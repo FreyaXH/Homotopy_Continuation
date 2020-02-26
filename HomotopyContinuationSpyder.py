01399e9c6efdc13876b559aa4b65cca32b68943c
Unknown <XH2416@ic.ac.uk>
2020-02-26 02:35:25 +0000

    Changing variable names
    note: 1d is wrong

--- HomotopyContinuationSpyder.py
+++ HomotopyContinuationSpyder.py
@@ -6,10 +6,11 @@ Created on Tue Feb 25 15:24:37 2020
 """
 
 #import functions
-import scipy as sp
+
 import numpy as np
 import sympy as sy
 import scipy.integrate as spi
+#import scipy.optimize as spo
 from sympy.abc import symbols
 from sympy.utilities.lambdify import lambdify 
 import itertools as it
@@ -23,29 +24,27 @@ import pandas as pd
 t,x,y, z, w, h, a,b,c,d, e, f, g,h = symbols('t,x,y, z, w, h, a,b,c,d, e,f,g,h', real = True)
 
 #construct homotopy
-def H(t, G, F, gamma):
+def Homotopy(t, G, F, gamma):
     return [(1 - t)*G[i] + gamma*t*F[i] for i in range(len(G))]
 
 #construct starting polynomial
-def G(x_array):
-    func_listG = [i**3 - 1 for i in x_array]
-    return func_listG
+def G(input_variables):
+    G_func = [i**3 - 1 for i in input_variables]
+    return G_func
 
 #generate gamma
-def gamma_generate():
+def Gamma_Generator():
     real = np.random.rand()
     im = np.sqrt(1- real**2)
     return real + im*1j
 
 #roots of startin function
-def G_roots_find(n):
+def G_Roots(n):
     root_list = [1, np.exp(1j*2*np.pi/3), np.exp(1j*2*np.pi*2/3)]
     return [i for i in it.product(root_list, repeat = n)]
 
 
-def Homotopy_Continuation(t, x_array, F_list, N, expansion_array, expansion_variables, tolerance = 10,\
-                          tolerance_zero = 1e-6, ratio_tolerance = 1e-10,\
-                          N_ratio_tolerance = 100, debug = False, corrector_Newtons = True, save_path = False, file_name = 'Homotopy_Roots'):
+def Homotopy_Continuation(t, input_variables, input_functions, number_of_steps, expanded_functions, expansion_variables, remainder_tolerance = 10,check_determinant_H = 1e-6, newton_ratio_accuracy = 1e-10,max_newton_step = 100, debug = False, Newtons_method = True, save_path = False, file_name = 'Homotopy_Roots'):
     
     """
     F = To be given as a list of variables, for example F = [x**2 , y**2], where the symbols used must first be
@@ -59,108 +58,108 @@ def Homotopy_Continuation(t, x_array, F_list, N, expansion_array, expansion_vari
     time_start = time.time()
     
     #convert F to a function
-    F = lambdify([x_array], F_list)
+    F = lambdify([input_variables], input_functions)
     
     #determine the worst accuracy
-    max_rem_total = 0
+    max_remainder_value = 0
     
     #count the number of roots
-    num = 0
-    delta_t = 1/N
-    n = len(x_array)
+    number_of_count = 0
+    delta_t = 1/number_of_steps
+    dimension = len(input_variables)
     
     #generate gamma
-    gamma = gamma_generate()
+    gamma = Gamma_Generator()
     
-    G_roots = G_roots_find(n)
+    G_roots = G_Roots(dimension)
     
     #construct homotopy
-    H_func = H(t, G(x_array), F(x_array), gamma)
+    H = Homotopy(t, G(input_variables), F(input_variables), gamma)
     
     #first derivative of H
-    H_diff_x = sy.Matrix([[H_func[i].diff(x_array[j]) for i in range(len(x_array))] for j in range(len(x_array))])
+    derivative_H_wrt_x = sy.Matrix([[H[i].diff(input_variables[j]) for i in range(len(input_variables))] for j in range(len(input_variables))])
     
     
-    determinant_H = H_diff_x.det()
+    determinant_H = derivative_H_wrt_x.det()
     
     #derivative with respect to t
-    H_diff_t = sy.Matrix([H_func[i].diff(t) for i in range(len(x_array))])
+    derivative_H_wrt_t = sy.Matrix([H[i].diff(t) for i in range(len(input_variables))])
     
     #check determinant is not zero so can invert
     if abs(determinant_H) == 0:
         raise TypeError('The determinant of H is zero!')
     
     #function of determinant H
-    det_H = lambdify((t, x_array), determinant_H)
+    determinant_H_func = lambdify((t, input_variables), determinant_H)
     
-    H_diff_x_inv = H_diff_x**-1
-    H_diff_inv = -H_diff_x_inv*H_diff_t
+    inverse_derivative_H_wrt_x = derivative_H_wrt_x**-1
+    inverse_derivative_H = -inverse_derivative_H_wrt_x*derivative_H_wrt_t
     
-    H_Hdiffprime = H_diff_x_inv*sy.Matrix(H_func)
+    H_over_derivative_H_wrt_x = inverse_derivative_H_wrt_x*sy.Matrix(H)
     
-    H_Hdiffprime = lambdify((t, x_array), [H_Hdiffprime[i] for i in range(len(H_Hdiffprime))])
+    H_over_derivative_H_wrt_x_func = lambdify((t, input_variables), [H_over_derivative_H_wrt_x[i] for i in range(len(H_over_derivative_H_wrt_x))])
         
-    H_prime = lambdify((t, x_array), [H_diff_inv[i] for i in range(len(H_diff_inv))])
+    inverse_derivative_H_func = lambdify((t, input_variables), [inverse_derivative_H[i] for i in range(len(inverse_derivative_H))])
     
-    Hprime_x = lambdify((x_array,t), [H_diff_x[i] for i in range(len(H_diff_x))])    
-    H_func_1d = lambdify([x_array,t], H_func)
+    #derivative_H_wrt_x_func = lambdify((input_variables,t), [derivative_H_wrt_x[i] for i in range(len(derivative_H_wrt_x))])    
+    #H_func = lambdify([input_variables,t], H)
     
 
-    x_old_arrays = []
-    x_olds = []
+    paths = []
+    solutions = []
     accuracies = []
     
     #run for all roots of starting system
     for x_old in G_roots:
-        x_old_array = []
-        num += 1
+        trace = []
+        number_of_count += 1
             
-        t_n = 0
+        t_new = 0
         
         #run for all steps starting at t=0 ending at t=1
-        while round(t_n,5) < 1:
-            x_old_array.append(x_old)
-            t_old = t_n
-            t_n += delta_t
+        while round(t_new,5) < 1:
+            trace.append(x_old)
+            t_old = t_new
+            t_new += delta_t
             
-            if n == 1:
-                sol = spi.solve_ivp(H_prime, (t_old, t_n), x_old)
-                sol_x = sol.y[-1][-1]
+            if dimension == 1:
+                predictor = spi.solve_ivp(inverse_derivative_H_func, (t_old, t_new), x_old)
+                predicted_solution = predictor.y[-1][-1]
                 
-                H_func_1 = lambdify((x,t), H_func_1d([x], t)[0])
-                Hprime_x_1 = lambdify((x,t), Hprime_x([x], t)[0])
+                #H_func_1d = lambdify((x,t), H_func([x], t)[0])
+                #derivative_H_wrt_x_func_1d = lambdify((x,t), derivative_H_wrt_x_func([x], t)[0])
 
-                #x_old = [spo.newton(H_func_1, sol_x, fprime = Hprime_x_1, args=(t_n, ))]
+                #x_old = [spo.newton(H_func_1d, predicted_solution, fprime = derivative_H_wrt_x_func_1d, args=(t_new, ))]
             
             else:
-                if abs(det_H(t_n, x_old)) < tolerance_zero:
+                if abs(determinant_H_func(t_new, x_old)) < check_determinant_H:
                     raise TypeError('The determinant of H is zero!')
                 
                 #perform RK4 method
-                x_old_predictor = x_old
-                sol = spi.solve_ivp(H_prime, (t_old, t_n), x_old)
-                sol_x = sol.y[:,-1]
+                #x_old_predictor = x_old
+                predictor = spi.solve_ivp(inverse_derivative_H_func, (t_old, t_new), x_old)
+                predicted_solution = predictor.y[:,-1]
                    
             #newton's method
-            x_old = sol_x
-            ratio = np.full(n, 1)
-            N_ratio = 0
-            delta = np.full(n, ratio_tolerance)
+            x_old = predicted_solution
+            ratio = np.full(dimension, 1)
+            number_of_newton_steps = 0
+            change_in_x = np.full(dimension, newton_ratio_accuracy)
             
-            if corrector_Newtons is True:
-                method_used = 'Newton-Raphson with ' + str(N_ratio_tolerance) + ' steps.'
+            if Newtons_method is True:
+                method_used = 'Newton-Raphson with ' + str(max_newton_step) + ' steps.'
                 time_newtons_start = time.time()
                 #tolerance criteria for step size in Newton's Method
-                while max(ratio) > ratio_tolerance and N_ratio < N_ratio_tolerance:
+                while max(ratio) > newton_ratio_accuracy and number_of_newton_steps < max_newton_step:
                     if debug: print("Before Newton", x_old)
-                    if abs(det_H(t_n, x_old)) < tolerance_zero:
+                    if abs(determinant_H_func(t_new, x_old)) < check_determinant_H:
                         raise TypeError('The determinant of H is zero!')
-                    x_old_intermediate = x_old - H_Hdiffprime(t_n, x_old)
-                    delta_old = delta
-                    delta = abs(x_old_intermediate - x_old)
-                    ratio = [delta[j]/(delta_old[j] + 1e-10) for j in range(n)]
+                    x_old_intermediate = x_old - H_over_derivative_H_wrt_x_func(t_new, x_old)
+                    change_in_x_old = change_in_x
+                    change_in_x = abs(x_old_intermediate - x_old)
+                    ratio = [change_in_x[j]/(change_in_x_old[j] + 1e-10) for j in range(dimension)]
                     x_old = x_old_intermediate
-                    N_ratio += 1
+                    number_of_newton_steps += 1
                     #x_old = spo.newton(H_func, sol_x, fprime = Hprime_x, args=(t_n, ))
                     
                     time_newtons_end = time.time()
@@ -171,26 +170,26 @@ def Homotopy_Continuation(t, x_array, F_list, N, expansion_array, expansion_vari
                     
             else:
                 method_used = 'Minuit'
-                if n == 1:
+                if dimension == 1:
                     raise TypeError('Minuit only runs for more than 1 dimension!')
                     
                 time_minuit_start = time.time()
                 #iminuit
-                H_func_t = H(t_n, G(expansion_array), F(expansion_array), gamma)
+                H_at_fixed_t = Homotopy(t_new, G(expanded_functions), F(expanded_functions), gamma)
                
-                if debug: print("Homotopy at current step: ", H_func_t)
+                if debug: print("Homotopy at current step: ", H_at_fixed_t)
                 
                 #split real and imaginary
-                H_im_real_array = sum([abs(sy.re(i_re)) for i_re in H_func_t] + [abs(sy.im(i_im)) for i_im in H_func_t])
+                H_im_real = sum([abs(sy.re(i_re)) for i_re in H_at_fixed_t] + [abs(sy.im(i_im)) for i_im in H_at_fixed_t])
                 
-                if debug: print("Homotopy Absolute value at current step: ", H_im_real_array)
+                if debug: print("Homotopy Absolute value at current step: ", H_im_real)
                 
-                H_func_t_combined = lambdify([expansion_variables], H_im_real_array)
+                H_im_real_func = lambdify([expansion_variables], H_im_real)
 
                 x_old_re_im = []
 
                 #split x_old to re and im
-                for i in range(n):
+                for i in range(dimension):
                     x_old_re_im.append(np.real(x_old[i]))
                     x_old_re_im.append(np.imag(x_old[i]))
                     
@@ -200,55 +199,55 @@ def Homotopy_Continuation(t, x_array, F_list, N, expansion_array, expansion_vari
                     
                 printlevel = 10 if debug else 0
                 
-                m = im.Minuit.from_array_func(H_func_t_combined, x_old_re_im, forced_parameters= string_variables,print_level=printlevel)
+                m = im.Minuit.from_array_func(H_im_real_func, x_old_re_im, forced_parameters= string_variables,print_level=printlevel)
                 m.migrad(resume=False)
 
                 x_old_im_re_vals = m.values
                 
                 
-                x_old = [x_old_im_re_vals[j] + 1j*x_old_im_re_vals[j+1] for j in range(0, 2*n, 2)]
+                x_old = [x_old_im_re_vals[j] + 1j*x_old_im_re_vals[j+1] for j in range(0, 2*dimension, 2)]
                 
                 if debug: print("After Minuit we got", x_old)
                 time_minuit_end = time.time()
                 
                 if debug:
                     print('Time for Minuit: {}'.format(time_minuit_end - time_minuit_start))
-
+            trace.append(x_old)    
         #check root is found
         remainder = list(map(abs, F(x_old))) 
         
-        if max(remainder) < tolerance:
-            x_old = [x_old[i].real if abs(x_old[i].imag) < tolerance_zero else x_old[i] for i in range(len(x_old))]
+        if max(remainder) < remainder_tolerance:
+            x_old = [x_old[i].real if abs(x_old[i].imag) < check_determinant_H else x_old[i] for i in range(len(x_old))]
             
             max_rem = max(remainder)
-            if max_rem_total < max_rem:
-                max_rem_total = max_rem
+            if max_remainder_value < max_rem:
+                max_remainder_value = max_rem
 
-            x_olds.append(x_old)
+            solutions.append(x_old)
             
             if save_path is True:
-                x_old_arrays.append(x_old_array)
+                paths.append(trace)
             accuracies.append(remainder)
 
     time_end = time.time()
     
     if save_path is False:
-        x_old_arrays = np.full(len(x_olds),'-')
+        paths = np.full(len(solutions),'-')
     
     
-    other_info = ['Function Used'] + F_list + [''] + ['Time Taken'] + [time_end - time_start] + [''] + \
-    ['Root Finding Method Used'] + [method_used] + [''] + ['Max Accuracy'] + [max_rem_total] + \
-    [''] + ['Number of Homotopy Steps'] + [N] 
+    other_info = ['Function Used'] + input_functions + [''] + ['Time Taken'] + [time_end - time_start] + [''] + \
+    ['Root Finding Method Used'] + [method_used] + [''] + ['Worst Accuracy'] + [max_remainder_value] + \
+    [''] + ['Number of Homotopy Steps'] + [number_of_steps] 
     
-    len_x_old = len(x_olds)
-    total_length = max(len(other_info), len_x_old)
+    len_solutions = len(solutions)
+    total_length = max(len(other_info), len_solutions)
     
     other_info = other_info + list(np.full(total_length - len(other_info), ''))
-    x_olds = x_olds + list(np.full(total_length - len_x_old, ''))
-    accuracies = accuracies + list(np.full(total_length - len_x_old, ''))
-    x_old_arrays = list(x_old_arrays) + list(np.full(total_length - len_x_old, ''))
-    
-    df = pd.DataFrame({'Roots' : x_olds, 'Accuracy' : accuracies, 'Paths' : x_old_arrays, 'Other Info' : other_info})
+    solutions = solutions + list(np.full(total_length - len_solutions, ''))
+    accuracies = accuracies + list(np.full(total_length - len_solutions, ''))
+    paths = list(paths) + list(np.full(total_length - len_solutions, ''))
+    df = pd.DataFrame({'Roots' : solutions, 'Accuracy' : accuracies, 'Paths' : paths, 'Other Info' : other_info})
     df.to_csv(file_name + '.csv', index=True)
-    return x_olds, x_old_arrays
+    return solutions, paths
+
 
