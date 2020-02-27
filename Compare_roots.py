f2b19ab22613c92f01ac62f1b2c38fb7045b0359
Shantralmalar Rasa <sr917@ic.ac.uk>
2020-02-27 22:25:35 +0000

    Function to compare roots in homotopy and  HOM4PS2 - incomplete

--- Compare_roots.py
+++ Compare_roots.py
@@ -8,9 +8,26 @@ Created on Thu Feb 27 20:06:51 2020
 import numpy as np
 import scipy as sp
 import pandas as pd
+import ast as a
 
-df = pd.read_csv('FHelp.csv')
+def obtain_roots_and_accuracy(filename):
+    df = pd.read_csv('FHelp.csv')
+    roots = df['Roots']
+    accuracy = df['Accuracy']
+    return roots, accuracy
 
-roots = df['Roots']
-#data = np.genfromtxt(r'FHelp.csv', delimiter=',', dtype = None)
-#print(data)
+def round_values(roots, decimal_places):
+    roots = [np.around(a.literal_eval(roots[i]), decimal_places) for i in range(len(roots))]
+    return roots
+
+def format_roots(filename, decimal_places):
+    datadf = pd.read_csv(filename, header=None)
+    
+    return [[np.around(complex(datadf[i][j][:-1] + 'j'), decimal_places) for j in range(len(datadf))] for i in datadf.columns]
+
+#def compare_values(roots_found, compare_roots):
+    #set(A) & set(B) 
+roots, accuracy = obtain_roots_and_accuracy('FHelp.csv')
+trial_roots = format_roots('try_homo2.csv', 3)
+roots = round_values(roots, 3)
+
